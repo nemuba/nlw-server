f242e2fc2c227a1a08ebe3f18e4776276acb39b5
Alef Ojeda de Oliveira <nemubatubag@gmail.com>
2020-06-28 15:54:50 -0300

    make all backend of the application

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,11 @@ import cors from 'cors';
 import routes from './routes';
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.resolve(__dirname,"..","uploads")));
-app.use(cors());
 
 app.listen(3333, () => {
   console.log("Listening on port 3333.");
